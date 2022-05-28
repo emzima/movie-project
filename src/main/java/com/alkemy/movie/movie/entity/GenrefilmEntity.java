14b61e71498237557d28308d02c964d878c4d849
emzima <ezapata.bnc@gmail.com>
2022-05-28 14:51:51 -0300

    version 3 updated

--- src/main/java/com/alkemy/movie/movie/entity/GenrefilmEntity.java
+++ src/main/java/com/alkemy/movie/movie/entity/GenrefilmEntity.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "genrefilm")
@@ -17,5 +19,8 @@ public class GenrefilmEntity {
     private String name_genre;
 
     private String image_genre;
+
+    @ManyToOne(fetch = FetchType.EAGER , cascade = CascadeType.ALL)
+    private List<MovieseriesEntity> movieseries = new ArrayList<>();
 
 }
